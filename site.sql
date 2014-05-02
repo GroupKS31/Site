8004096dfd3962305b803cf36d077e602eef98c5
Cronverk <pozniack@gmail.com>
2014-05-02 20:51:20 +0300

    m1

--- site.sql
+++ site.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Май 02 2014 г., 15:52
+-- Время создания: Май 02 2014 г., 20:49
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -31,8 +31,16 @@ CREATE TABLE IF NOT EXISTS `area` (
   `name` varchar(20) NOT NULL,
   `address` varchar(50) NOT NULL,
   `town` varchar(20) NOT NULL,
+  `pole_count` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Дамп данных таблицы `area`
+--
+
+INSERT INTO `area` (`id`, `name`, `address`, `town`, `pole_count`) VALUES
+(1, 'PAPA ROACH COVER PAR', ' ул.Плехановская 134а,метро Московский проспект', 'Харьков', 3);
 
 -- --------------------------------------------------------
 
@@ -78,7 +86,18 @@ CREATE TABLE IF NOT EXISTS `concert` (
   `time` time DEFAULT NULL,
   `manager_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Дамп данных таблицы `concert`
+--
+
+INSERT INTO `concert` (`id`, `name`, `band_id`, `area_id`, `date`, `time`, `manager_id`) VALUES
+(1, 'example', 1, 1, '2014-05-28', '20:00:00', 1),
+(2, 'example1', 2, 1, '0000-00-00', '00:00:00', 1),
+(3, 'example3', 3, 1, '0000-00-00', '00:00:00', 1),
+(4, 'example4', 4, 1, '0000-00-00', '00:00:00', 1),
+(5, 'example5', 5, 1, '0000-00-00', '00:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -109,14 +128,62 @@ CREATE TABLE IF NOT EXISTS `picture` (
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `place`
+--
+
+CREATE TABLE IF NOT EXISTS `place` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sector_id` int(11) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  `serial` int(10) DEFAULT NULL,
+  `flag` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+
+--
+-- Дамп данных таблицы `place`
+--
+
+INSERT INTO `place` (`id`, `sector_id`, `number`, `serial`, `flag`) VALUES
+(1, 1, 0, 213123, 0),
+(2, 1, 0, 213123, 0),
+(3, 1, 0, 213123, 0),
+(4, 1, 0, 213123, 0),
+(5, 1, 0, 213123, 0),
+(6, 1, 0, 213123, 0),
+(7, 2, 0, 1231212, 0),
+(8, 2, 0, 1231212, 0),
+(9, 2, 0, 1231212, 0),
+(10, 2, 0, 1231212, 0),
+(11, 2, 0, 1231212, 0),
+(12, 2, 0, 1231212, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `sector`
 --
 
 CREATE TABLE IF NOT EXISTS `sector` (
-  `id` int(11) NOT NULL,
-  `area_id` int(11) NOT NULL,
-  `pole_number` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(7) DEFAULT NULL,
+  `Place_cost` double DEFAULT NULL,
+  `area_id` int(11) DEFAULT NULL,
+  `pole_number` int(11) NOT NULL,
+  `position` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Дамп данных таблицы `sector`
+--
+
+INSERT INTO `sector` (`id`, `name`, `Place_cost`, `area_id`, `pole_number`, `position`) VALUES
+(1, 'A', 100, 1, 1, 1),
+(2, 'B', 250, 1, 1, 2),
+(3, 'C', 300, 1, 2, 1),
+(4, 'D', 50, 1, 2, 2),
+(5, 'Stage', 0, 1, 3, 123);
 
 -- --------------------------------------------------------
 
